f9c394900741ba80c6491b457bbe2f7f1953e5f0
JiHyoKun93 <124251690+JiHyoKun93@users.noreply.github.com>
2023-09-22 19:26:25 +0900

    채팅시스템 기본 완성

--- FinalProject/src/main/java/com/spring/boot/chat/WebSocketConfig.java
+++
@@ -1,9 +0,0 @@
-package com.spring.boot.chat;
-
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-
-
-@EnableWebSocketMessageBroker
-public class WebSocketConfig {
-    
-}
