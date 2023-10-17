5ff7e6af76199265d918099e9823ab9ac6168af7
eunsssss <eslee1270@gmail.com>
2023-10-17 11:12:54 +0900

    11

--- FinalProject/src/main/java/com/spring/boot/controller/MeetmateController.java
+++ FinalProject/src/main/java/com/spring/boot/controller/MeetmateController.java
@@ -192,7 +192,9 @@ public class MeetmateController {
 		// 		searchValue = URLDecoder.decode(searchValue, "UTF-8");
 		// 	}
 		// }   ******************************************
-
+		
+		System.out.println("searchKey 내용 : " + searchKey);
+		System.out.println("searchValue 내용 : " + searchValue);
 
 		//meetMateLists = gatchiService.getMeetMateListsPaging(start, itemsPerPage);//*****
 
@@ -201,8 +203,49 @@ public class MeetmateController {
 		meetMateSlideLists = gatchiService.getMeetMateRandomList(3); // 5개의 랜덤 모임을 가져옴
 
 		//System.out.println("모임 DB 가져온 내용 : " + meetLists);
+
+
+
+		mav.addObject("meetMateSlideLists", meetMateSlideLists);
+		
+		mav.addObject("meetLists", meetMateLists);
+		//mav.addObject("readData", readData);//추가한거
+		mav.setViewName("/meetmate/meetMateList");
+		
+		return mav;		
+	}
+
+	@PostMapping("/meetMateList")
+	public ModelAndView meetMateList(@RequestParam(name = "searchKey", required = false) String searchKey,
+        @RequestParam(name = "searchValue", required = false) String searchValue) throws Exception {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		List<GatchiDTO> meetMateLists = new ArrayList<>();
+		List<GatchiDTO> meetMateSlideLists = new ArrayList<>();
+		
+		// // String searchKey = request.getParameter("searchKey");
+		// // String searchValue = request.getParameter("searchValue");
+		// if (searchValue == null) {
+		// 	searchKey = "meetTitle";
+		// 	searchValue = "";
+		
+		// } else {
+		// 	if (request.getMethod().equalsIgnoreCase("GET")) {
+		// 		searchValue = URLDecoder.decode(searchValue, "UTF-8");
+		// 	}
+		// }   ******************************************
+		
 		System.out.println("searchKey 내용 : " + searchKey);
 		System.out.println("searchValue 내용 : " + searchValue);
+
+		//meetMateLists = gatchiService.getMeetMateListsPaging(start, itemsPerPage);//*****
+
+		//meetMateLists = gatchiService.getMeetMateLists(); //***************
+		meetMateLists = gatchiService.searchMeetMateList(searchKey, searchValue);
+		meetMateSlideLists = gatchiService.getMeetMateRandomList(3); // 5개의 랜덤 모임을 가져옴
+
+		//System.out.println("모임 DB 가져온 내용 : " + meetLists);
 
 
 
