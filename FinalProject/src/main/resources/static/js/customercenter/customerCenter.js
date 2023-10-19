1777097d6c3027a69d935dfd84a191986cb35c02
djwotjr5667 <djwotjr5667@naver.com>
2023-10-19 17:21:31 +0900

    설명 추가

--- FinalProject/src/main/resources/static/js/customercenter/customerCenter.js
+++ FinalProject/src/main/resources/static/js/customercenter/customerCenter.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 버튼 클릭 이벤트 핸들러
+    // 버튼 클릭 시 해당하는 주제의 질문만 출력
     document.querySelectorAll('button[data-subject]').forEach(button => {
         button.addEventListener('click', function() {
             const selectedSubject = this.getAttribute('data-subject');
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    // 검색 기능
+    // 검색어를 입력해서 해당 검색어가 포함된 질문과 답변 모두 출력
     document.getElementById('searchBtn').addEventListener('click', function() {
         const searchQuery = document.getElementById('searchInput').value.toLowerCase();
 
