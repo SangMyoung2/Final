4416c30580d8b06297b5c4d6ac9337e0c503efce
eunsssss <eslee1270@gmail.com>
2023-10-21 13:40:10 +0900

    커밋

--- FinalProject/src/main/java/com/spring/boot/mapper/GatchiMapper.java
+++ FinalProject/src/main/java/com/spring/boot/mapper/GatchiMapper.java
@@ -19,7 +19,7 @@ public interface GatchiMapper {
 
 	public String masterPicture() throws Exception;
 	
-	// public GatchiDTO getReadData(int meetListNum) throws Exception;
+	public GatchiDTO getReadData(int meetListNum) throws Exception;
 
 	public List<GatchiDTO> getMeetMateLists() throws Exception;
 
@@ -30,35 +30,20 @@ public interface GatchiMapper {
 	public List<GatchiDTO> getCommuniFindRandomList(int count) throws Exception;
 
     public List<GatchiDTO> searchMeetMateList(@Param("searchKey")String searchKey, @Param("searchValue")String searchValue);
-	
+
     public List<GatchiDTO> searchCommuniFindList(@Param("searchKey")String searchKey, @Param("searchValue")String searchValue);
-	
+
 	public List<GatchiDTO> getRownumList(int end) throws Exception;
 	
-	public void updateMeetStatusMate(GatchiDTO gatchiDTO) throws Exception;
-	
-	public void updateMeetStatusFind(GatchiDTO gatchiDTO) throws Exception;
-
-	public List<GatchiDTO> sortByLikeCountMeet() throws Exception;
-
-	public List<GatchiDTO> sortByHitCountMeet() throws Exception;
-
-	public List<GatchiDTO> sortByDdayMeet() throws Exception;
-
-	public List<GatchiDTO> sortByLikeCountFind() throws Exception;
-
-	public List<GatchiDTO> sortByHitCountFind() throws Exception;
-
-	public List<GatchiDTO> sortByDdayFind() throws Exception;
-
     //public void meetLikes(int meetListNum) throws Exception;
 
 	// public int getDataCount(String searchKey, String searchValue) throws Exception;
 	
 	// public List<MeetmateDTO> getLists(int start, int end, String searchKey, String searchValue);
+		
+	public void updateHitCount(int meetListNum) throws Exception;	
 	
-	// public void updateHitCount(int num) throws Exception;	
-	
+	// public void updateData(MeetmateDTO dto) throws Exception;
 	
 	// public void deleteData(int num) throws Exception;
     
