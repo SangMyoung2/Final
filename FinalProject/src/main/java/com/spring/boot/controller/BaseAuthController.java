67be3d23ce2fc1b9f8e8588393b02ca47c0a8426
sw <tkddn00001@naver.com>
2023-10-21 13:24:52 +0900

    dd

--- FinalProject/src/main/java/com/spring/boot/controller/BaseAuthController.java
+++ FinalProject/src/main/java/com/spring/boot/controller/BaseAuthController.java
@@ -1,8 +1,17 @@
 package com.spring.boot.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
-import javax.annotation.Resource;
+import java.util.UUID;
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +19,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import com.spring.boot.dao.UserRepository;
 import com.spring.boot.model.Users;
@@ -20,7 +30,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BaseAuthController {
 
-	@Resource
+	@Autowired
 	private UserService userService;
 
    @Autowired
@@ -56,13 +66,15 @@ public class BaseAuthController {
 	}
 	
 	@PostMapping("/signup.action")
-	public String signup(Users dto, BindingResult bindResult,Model model,@RequestParam("userName") String userName) {
+	public String signup(Users dto, BindingResult bindResult,Model model,
+	@RequestParam("userName") String userName,
+	@RequestParam("picture") String picture) {
 		
 
 		try {
 			
 			userService.create(dto.getUserName(), dto.getName(),
-					dto.getPassword(),dto.getTel());
+					dto.getPassword(),dto.getTel(),dto.getPicture());
 
 					
 			return "login/signup_ok";
@@ -177,6 +189,46 @@ public class BaseAuthController {
 	return mav;
 	}
 
+
+	@GetMapping("/userupdate.action")
+	public ModelAndView userupdate() {
+		ModelAndView mav = new ModelAndView();
+		
+	mav.setViewName("login/userupdate");
+			
+	return mav;
+	}
+
+
+	@PostMapping("/userupdate.action")
+	public ModelAndView userupdate_ok(
+		@RequestParam("email") String email,
+		@RequestParam("name") String name,
+		@RequestParam("password") String password,
+		 @RequestParam("userImg") MultipartFile userImg) throws IOException {
+
+
+			ModelAndView mav = new ModelAndView();
+
+		// Resource resource = new ClassPathResource("static");
+      	// String resourcePath = resource.getFile().getAbsolutePath() + "\\image\\login";
+		  String resourcePath = "C:\\VSCode\\Final\\FinalProject\\src\\main\\resources\\static\\image\\login";
+		
+		
+
+		String originalFilename = userImg.getOriginalFilename();
+				String saveFileName = originalFilename + UUID.randomUUID();
+				Path filePath = Paths.get(resourcePath, saveFileName);
+            	
+				
+            	Files.write(filePath, userImg.getBytes());
+				
+			userService.userupDate(email,password,name,saveFileName);
+		
+		mav.setViewName("login/userupdate_ok");
+				
+		return mav;
+		}
 
 
 	
--- FinalProject/src/main/resources/static/js/signup/mypage.js
+++ FinalProject/src/main/resources/static/js/signup/mypage.js
@@ -1,10 +1,12 @@
 // 버튼을 클릭할 때 Ajax 요청을 보내는 함수
-function sendAjaxRequest(endpoint) {
-    event.preventDefault();
+function sendRequest(endpoint) {
     $.ajax({
         url: endpoint,
         method: 'GET',
         dataType: 'json',
+        data:{
+            
+        },
         success: function(data) {
             updateUI(data);
         },
@@ -14,32 +16,39 @@ function sendAjaxRequest(endpoint) {
     });
 }
 
-// MeetMate 버튼 클릭 시 Ajax 요청 보내기
-$('#meetmate-link').on('click', function(event) {
-    sendAjaxRequest('/api/meetmate');
-});
-
-// CommuniFind 버튼 클릭 시 Ajax 요청 보내기
-$('#communifind-link').on('click', function(event) {
-    sendAjaxRequest('/api/communifind');
-});
-
-// Challenge 버튼 클릭 시 Ajax 요청 보내기
-$('#challenge-link').on('click', function(event) {
-    sendAjaxRequest('/api/challenge');
-});
-
-// My Likes 버튼 클릭 시 Ajax 요청 보내기
-$('#mylikes-link').on('click', function(event) {
-    sendAjaxRequest('/api/mylikes');
-});
-
-// My Chat 버튼 클릭 시 Ajax 요청 보내기
-$('#mychat-link').on('click', function(event) {
-    sendAjaxRequest('/api/mychat');
-});
 
 function updateUI(data) {
-    // 받은 데이터를 사용하여 페이지의 일부분을 동적으로 업데이트
-    // 예: DOM 조작을 통해 새로운 콘텐츠를 페이지에 추가
+   
 }
+
+
+$('#meetmate-link').on('click', function(event) {
+    event.preventDefault();
+    sendRequest('/meetmate');
+});
+
+
+$('#communifind-link').on('click', function(event) {
+    event.preventDefault();
+    sendRequest('/communifind');
+});
+
+
+$('#challenge-link').on('click', function(event) {
+    event.preventDefault();
+    sendRequest('/challenge');
+});
+
+
+$('#mylikes-link').on('click', function(event) {
+    event.preventDefault();
+    sendRequest('/mylikes');
+});
+
+
+$('#mychat-link').on('click', function(event) {
+    event.preventDefault();
+    sendRequest('/mychat');
+});
+
+
