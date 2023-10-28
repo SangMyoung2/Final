c0545bbdf14ca591d08100374242e1aeb990b908
djwotjr5667 <djwotjr5667@naver.com>
2023-10-28 14:52:32 +0900

    충전내역/사용내역 없을 때의 필터링 구현

--- FinalProject/src/main/resources/static/js/pay/paymentInfo.js
+++ FinalProject/src/main/resources/static/js/pay/paymentInfo.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     // 초기 설정
-    $('.paymentinfocontainer').show();
-    $('.pointinfocontainer').hide();
+    checkAndDisplayNoResultsMessage();
 
     // 포인트 충전 내역 버튼 클릭 시
     $('#show-pay-history').click(function() {
@@ -9,6 +8,7 @@ $(document).ready(function() {
         $('.paymentinfocontainer').show();
         $('.pointinfocontainer').hide();
         $('.paytitle').text('포인트 충전 내역');
+        checkAndDisplayNoResultsMessage();
     });
 
     // 포인트 사용 내역 버튼 클릭 시
@@ -17,8 +17,10 @@ $(document).ready(function() {
         $('.paymentinfocontainer').hide();
         $('.pointinfocontainer').show();
         $('.paytitle').text('포인트 사용 내역');
+        checkAndDisplayNoResultsMessage();
     });
 
+    // 필터 적용
     $('#filter-btn').click(function() {
         var startDateInput = $('#start-date').val();
         var endDateInput = $('#end-date').val();
@@ -26,7 +28,6 @@ $(document).ready(function() {
         var endDate = new Date(endDateInput).getTime();
 
         resetFilters();
-
         if ($('.paytitle').text() === '포인트 충전 내역') {
             filterResults('.paymentinfocontainer', '.label:contains("결제 날짜/시간:")', startDate, endDate);
         } else {
@@ -60,21 +61,29 @@ $(document).ready(function() {
         }
     }
 
+    function checkAndDisplayNoResultsMessage() {
+        if ($('.paymentinfocontainer:visible').length === 0 && $('.paytitle').text() === '포인트 충전 내역') {
+            $('#no-payment-results-message').show();
+        } else if ($('.pointinfocontainer:visible').length === 0 && $('.paytitle').text() === '포인트 사용 내역') {
+            $('#no-use-results-message').show();
+        } else {
+            $('#no-payment-results-message, #no-use-results-message').hide();
+        }
+    }
+
     // useType 값에 따른 텍스트 변환
     $('.pointinfocontainer').each(function() {
-    var useTypeText = $(this).find('.label:contains("구분:")').next().text();
-
-    switch(useTypeText) {
-        case '1':
-            $(this).find('.label:contains("구분:")').next().text('사용');
-            break;
-        case '2':
-            $(this).find('.label:contains("구분:")').next().text('충전');
-            break;
-        case '3':
-            $(this).find('.label:contains("구분:")').next().text('환불');
-            break;
-    }
-});
-
+        var useTypeText = $(this).find('.label:contains("구분:")').next().text();
+        switch(useTypeText) {
+            case '1':
+                $(this).find('.label:contains("구분:")').next().text('사용');
+                break;
+            case '2':
+                $(this).find('.label:contains("구분:")').next().text('충전');
+                break;
+            case '3':
+                $(this).find('.label:contains("구분:")').next().text('환불');
+                break;
+        }
+    });
 });
